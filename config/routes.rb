8e6f1a98df60545dd4fa40393725fc12cd257027
何永东 <heyongdonghe@qq.com>
2015-07-04 21:18:27 +0800

    静态页面添加控制器
    静态页面控制器static_pages添加(包home,help方法

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   devise_for :people
   resources :users
 
@@ -15,6 +19,7 @@ Rails.application.routes.draw do
   get 'activities/show'
 
   delete 'activities/destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
