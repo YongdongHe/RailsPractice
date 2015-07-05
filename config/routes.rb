5b64a62e917cf481f9f4f97ebcad84fb3ede04a1
何永东 <heyongdonghe@qq.com>
2015-07-05 14:31:05 +0800

    加入bootstrap模板的菜单栏
    修改/help路由的匹配,将其转向/static_pages#help，添加via:'get'
    把about转向static_pages/about

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
+
 Rails.application.routes.draw do
-  root :to =>"static_pages#home"
+  root :to =>"home#home"
+  # 或者使用 match "/" ,to: 'home#home' , via:'get'也可以达到相同的效果
+
+  get 'home/home'
+
   get 'static_pages/home'
 
+#添加/help路由，转向static_pages#help，使得更简洁
   get 'static_pages/help'
+  match '/test', to:'static_pages#testway' , via: 'get'
 
   get 'static_pages/about'
 
