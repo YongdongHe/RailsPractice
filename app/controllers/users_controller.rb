e863a9e63ee8d4505def79617c35ba886abc919c
何永东 <heyongdonghe@qq.com>
2015-07-06 15:49:15 +0800

    输入名字查找邮箱

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,19 +23,17 @@ class UsersController < ApplicationController
 
   def search
     # @user_id = params[:test]
-    @user_id = params[:user_id]
-
-    u = User.find(@user_id)
-    data = {json1: 'ok',one: 'false'}
+    @user_name = params[:user_name]
+    u = User.find_by(:name => @user_name)
     respond_to do |format|
-      # if u
-      #   data = {'json'=>'json_data',:name => u.name ,id: @user_id , :status => 'success'}
-      #   format.json { render json: data }
-      # else
-      #   data = {:status => 'fail'}
-      #   format.json { render json: data}
-      # end
-      format.json { render json: data }
+      if u!=nil
+        data = {'json'=>'json_data',:name => u.name ,id: u.id,:email => u.email, :status => 'success'}
+        format.json { render json: data }
+      else
+        data = {:status => 'fail'}
+        format.json { render json: data}
+      end
+      # format.json { render json: data }
     end
 
     # respond_to do |format|
