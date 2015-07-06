5a45c59025294c91d55a94828e4fd699e0574f01
何永东 <heyongdonghe@qq.com>
2015-07-06 22:52:36 +0800

    Revert "数据库唯一性认证"
    This reverts commit 2c95824bffb9a8da75c6a6c35157b7f0c9ff9795.
    
    Conflicts:
    	app/models/user.rb
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706143206) do
+ActiveRecord::Schema.define(version: 20150706141817) do
 
   create_table "activities", force: true do |t|
     t.string   "topic"
@@ -52,8 +52,6 @@ ActiveRecord::Schema.define(version: 20150706143206) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.string   "password"
-    t.string   "password_confirmation"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
