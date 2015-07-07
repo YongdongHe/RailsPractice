7714c992c24ce7344c1b6e935e6517c1baefa76b
何永东 <heyongdonghe@qq.com>
2015-07-07 20:47:37 +0800

    修复symbol cannot change to int

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,9 @@
 class SessionsController < ApplicationController
 	
 	def create
-		render 'new'
-		user_email = :params[:session][:email]
-		user_password = :params[:session][:passwor]
-		user = User.find_by(:email => user_email)
-		if user && user.authenticate(user_password)
+		render 'new'		
+		user = User.find_by( :email  => params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
 			#密码正确
 			flash.now[:success] = '登陆成功'
 			#下面的还未定义
