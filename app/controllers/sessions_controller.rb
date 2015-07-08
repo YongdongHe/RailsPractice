9cf691f570c36beec2115831f64ead1309468f70
何永东 <heyongdonghe@qq.com>
2015-07-08 10:15:56 +0800

    Session修改，登陆成功后跳转到资料页面
    Redirect to user可以直接跳转

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,17 @@
 class SessionsController < ApplicationController
 	
-	def create
-		render 'new'		
+	def create			
 		user = User.find_by( :email  => params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			#密码正确
 			flash.now[:success] = '登陆成功'
 			#下面的还未定义
 			sign_in user
+			puts user
 			redirect_to user
-
 		else
 			flash.now[:error] = '登陆失败'
+			render 'new'	
 		end
 	end
 
