75258510847c9f8301d5d479e13073dd16a40424
何永东 <heyongdonghe@qq.com>
2015-07-08 11:32:26 +0800

    登出功能，教程上需要修改
    def sign_out
    remember_token = User.hash(cookies[:remember_token])
    current_user = User.find_by(:remember_token => remember_token)
    current_user.update_attribute(:remember_token ,
    User.hash(remember_token))
    self.current_user = nil
    cookies.delete :remember_token
    end
    
    还是要依然根据cookies获取当前登陆的用户，如果直接用current_usre或者@current_user取出来是空的

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,14 @@ module SessionsHelper
 		self.current_user = user
 	end
 
+	def sign_out
+		remember_token = User.hash(cookies[:remember_token])
+		current_user = User.find_by(:remember_token => remember_token)
+		current_user.update_attribute(:remember_token , User.hash(remember_token))
+		self.current_user = nil
+		cookies.delete :remember_token
+	end
+
 	def signed_in?
 		!current_user.nil?
 	end
